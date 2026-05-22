1c650fba6b88ad6134ac161c1b22fec9e760c05a
Jchips <20979096+Jchips@users.noreply.github.com>
2026-05-21 22:14:33 -0700

    removed @babel/plugin-proposal-optional-chaining-assign package

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      ['@babel/plugin-proposal-optional-chaining-assign',
-        {
-          "version": "2023-07"
-        }]
     ],
   };
 };
